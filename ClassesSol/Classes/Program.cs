c45a79c1675fcba401b19acbaef1c448e00862e9
Saurabh Mishra <backtohome924@gmail.com>
2022-07-07 23:25:03 +0530

    added Object files and Class file.

--- ClassesSol/Classes/Program.cs
+++ ClassesSol/Classes/Program.cs
@@ -6,7 +6,38 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car myCar = new Car();
+            myCar.Make = "Mahindra";
+            myCar.Model = "XUV700";
+            myCar.Year = 2022;
+            myCar.Color = "Midnight Blue";
+                                   
+            Console.WriteLine("Car details are : {0} {1} {2}, Comapany : {3} ", myCar.Model, myCar.Year, myCar.Color, myCar.Make);
+            Console.WriteLine("{0:N}",myCar.DetermineMarketValue());
+            
+            Console.ReadLine();
+        }
+            
+    }
+
+    class Car
+    {
+        //class is the blue print and object is instantiation 
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public  decimal DetermineMarketValue()
+        {
+            decimal carValue;
+            if (Year >= 2022)
+            {
+                carValue = 22000;
+            }
+            else 
+                carValue = 15000;
+            return carValue;
         }
     }
 }
--- ObjectLifeTimeSol/ObjectLifeTime/Program.cs
+++ ObjectLifeTimeSol/ObjectLifeTime/Program.cs
@@ -6,7 +6,72 @@ namespace ObjectLifeTime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int[] arr = new int[] { 4, 2, 6, 8, 1, 0 };
+            //SelectionSort(arr);
+            
+            //int[] arr = new int[] { 10,2,7,11,15};
+           // int target = 9;
+            //if (TwoSum(arr, 9))
+            //{
+            //    Console.WriteLine("Valid pair exists");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No valid pair exists for " + target);
+            //}
+
+
+            Console.ReadLine();
+        }
+
+        private static void SelectionSort(int[] nums)
+        {
+
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                int MinValue = i;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[j] < nums[MinValue])
+                        MinValue = j;
+                }
+                int temap = nums[i];
+                nums[i] = nums[MinValue];
+                nums[MinValue] = temap;
+
+            }
+            foreach (var items in nums)
+            {
+                Console.WriteLine(items);
+            }
+        }
+        public static bool TwoSum(int[] nums, int target)
+        {
+            //[2,7,11,15]
+            int[] indices = new int[2];
+            int size = nums.Length;
+            for (int i = 0; i < (size - 1); i++)
+            {
+                for (int j = (i + 1); j < size; j++)
+                {
+                    
+                    if (nums[i] + nums[j] == target)
+                    {
+                        Console.WriteLine("Pair with a given sum " + target +
+                                           " is (" + nums[i] + ", " + nums[j] + ")");
+                        indices[0] = Array.IndexOf(nums, nums[i]);
+                        indices[1] = j;
+                        Console.WriteLine(indices[0]);
+                        Console.WriteLine(indices[1]);
+                        return true;
+                    }
+                }
+
+               
+            }
+            
+            return false;
         }
     }
 }
