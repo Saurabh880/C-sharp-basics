249f54dbe7cebf2041d20d0e43200a63ea278fca
Saurabh Mishra <backtohome924@gmail.com>
2022-07-21 19:46:47 +0530

    Binary search

---
+++ BinarySearchSol/BinarySearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 3, 5 };
+            int size = arr.Length;
+            int target = 3;
+
+            int start = 0;
+            int end = arr.Length - 1;
+            int mid = start + (end - start) / 2;
+            int ans = -1;
+            while (start < end)
+            {
+                if (arr[mid] == target)
+                {
+                    ans = mid;
+                    end = mid - 1;
+                }
+                else if (arr[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else if(arr[mid] < target)
+                {
+                    start = mid + 1;
+                }
+                mid = start + (end - start) / 2;
+            }
+            Console.WriteLine("first occurence of 3 is at index",ans);
+        }
+    }
+}
